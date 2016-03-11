8b20193652ff1f8c668e74011a7e80da0733226d
ektor5 <ek5.chimenti@gmail.com>
2016-03-11 15:44:43 +0100

    Disable deploying for now

--- deploy.sh
+++ deploy.sh
@@ -23,28 +23,28 @@ git commit -m "Deploy to Arduino branch"
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:arduino > /dev/null 2>&1
 
-cd ..
-
-git pull
-mkdir deploy
-
-DOWNLOAD_URL="https://udooboard.github.io/udooneo-arduino-libraries"
-./publish.sh origin/arduino "$DOWNLOAD_URL" deploy
-
-cd deploy
-git init
-
-# inside this git repo we'll pretend to be a new user
-git config user.name "Travis CI"
-git config user.email "social@udoo.org"
-
-# The first and only commit to this new Git repo contains all the
-# files present with the commit message "Deploy to GitHub Pages".
-git add .
-git commit -m "Deploy to GitHub Pages"
-
-# Force push from the current repo's master branch to the remote
-# repo's gh-pages branch. (All previous history on the gh-pages branch
-# will be lost, since we are overwriting it.) We redirect any output to
-# /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+#cd ..
+#
+#git pull
+#mkdir deploy
+#
+#DOWNLOAD_URL="https://udooboard.github.io/udooneo-arduino-libraries"
+#./publish.sh origin/arduino "$DOWNLOAD_URL" deploy
+#
+#cd deploy
+#git init
+#
+## inside this git repo we'll pretend to be a new user
+#git config user.name "Travis CI"
+#git config user.email "social@udoo.org"
+#
+## The first and only commit to this new Git repo contains all the
+## files present with the commit message "Deploy to GitHub Pages".
+#git add .
+#git commit -m "Deploy to GitHub Pages"
+#
+## Force push from the current repo's master branch to the remote
+## repo's gh-pages branch. (All previous history on the gh-pages branch
+## will be lost, since we are overwriting it.) We redirect any output to
+## /dev/null to hide any sensitive credential data that might otherwise be exposed.
+#git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
